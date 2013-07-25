ed01d73889cf86fbbf1946624c9f541188976f96
Victor Galkin <vicglarson@gmail.com>
2013-07-25 23:45:01 +0400

    Remove folsom config because grizzly config is common for folsom and grizzly

--- prepare.py
+++ prepare.py
@@ -9,190 +9,6 @@ import argparse
 
 CIRROS_IMAGE = 'https://launchpad.net/cirros/trunk/0.3.0/+download/cirros-0.3.0-x86_64-disk.img'
 
-template_folsom = """
-[identity]
-# This section contains configuration options that a variety of Tempest
-# test clients use when authenticating with different user/tenant
-# combinations
-
-# Set to True if your test environment's Keystone authentication service should
-# be accessed over HTTPS
-use_ssl = %(IDENTITY_USE_SSL)s
-# This is the main host address of the authentication service API
-host = %(IDENTITY_HOST)s
-# Port that the authentication service API is running on
-port = %(IDENTITY_PORT)s
-# Version of the authentication service API (a string)
-api_version = %(IDENTITY_API_VERSION)s
-# Path to the authentication service tokens resource (do not modify unless you
-# have a custom authentication API and are not using Keystone)
-path = %(IDENTITY_PATH)s
-# Should typically be left as keystone unless you have a non-Keystone
-# authentication API service
-strategy = %(IDENTITY_STRATEGY)s
-
-[compute]
-# This section contains configuration options used when executing tests
-# against the OpenStack Compute API.
-
-# Allows test cases to create/destroy tenants and users. This option
-# enables isolated test cases and better parallel execution,
-# but also requires that OpenStack Identity API admin credentials
-# are known.
-allow_tenant_isolation = %(COMPUTE_ALLOW_TENANT_ISOLATION)s
-
-# Allows test cases to create/destroy tenants and users. This option
-# enables isolated test cases and better parallel execution,
-# but also requires that OpenStack Identity API admin credentials
-# are known.
-allow_tenant_reuse = %(COMPUTE_ALLOW_TENANT_REUSE)s
-
-# This should be the username of a user WITHOUT administrative privileges
-username = %(USERNAME)s
-# The above non-administrative user's password
-password = %(PASSWORD)s
-# The above non-administrative user's tenant name
-tenant_name = %(TENANT_NAME)s
-
-# This should be the username of an alternate user WITHOUT
-# administrative privileges
-alt_username = %(ALT_USERNAME)s
-# The above non-administrative user's password
-alt_password = %(ALT_PASSWORD)s
-# The above non-administrative user's tenant name
-alt_tenant_name = %(ALT_TENANT_NAME)s
-
-# Reference data for tests. The ref and ref_alt should be
-# distinct images/flavors.
-image_ref = %(IMAGE_ID)s
-image_ref_alt = %(IMAGE_ID_ALT)s
-flavor_ref = %(FLAVOR_REF)s
-flavor_ref_alt = %(FLAVOR_REF_ALT)s
-
-# Number of seconds to wait while looping to check the status of an
-# instance that is building.
-build_interval = %(COMPUTE_BUILD_INTERVAL)s
-
-# Number of seconds to time out on waiting for an instance
-# to build or reach an expected status
-build_timeout = %(COMPUTE_BUILD_TIMEOUT)s
-
-# Run additional tests that use SSH for instance validation?
-# This requires the instances be routable from the host
-#  executing the tests
-run_ssh = %(RUN_SSH)s
-network_for_ssh = %(NETWORK_FOR_SSH)s
-
-# The type of endpoint for a Compute API service. Unless you have a
-# custom Keystone service catalog implementation, you probably want to leave
-# this value as "compute"
-catalog_type = %(COMPUTE_CATALOG_TYPE)s
-
-# Does the Compute API support creation of images?
-create_image_enabled = %(COMPUTE_CREATE_IMAGE_ENABLED)s
-
-# For resize to work with libvirt/kvm, one of the following must be true:
-# Single node: allow_resize_to_same_host=True must be set in nova.conf
-# Cluster: the 'nova' user must have scp access between cluster nodes
-resize_available = %(COMPUTE_RESIZE_AVAILABLE)s
-
-# Does the compute API support changing the admin password?
-change_password_available = %(COMPUTE_CHANGE_PASSWORD_AVAILABLE)s
-
-# Level to log Compute API request/response details.
-log_level = %(COMPUTE_LOG_LEVEL)s
-
-# Whitebox options for compute. Whitebox options enable the
-# whitebox test cases, which look at internal Nova database state,
-# SSH into VMs to check instance state, etc.
-
-# Should we run whitebox tests for Compute?
-whitebox_enabled = %(COMPUTE_WHITEBOX_ENABLED)s
-
-# Path of nova source directory
-source_dir = %(COMPUTE_SOURCE_DIR)s
-
-# Path of nova configuration file
-config_path = %(COMPUTE_CONFIG_PATH)s
-
-# Directory containing nova binaries such as nova-manage
-bin_dir = %(COMPUTE_BIN_DIR)s
-
-# Path to a private key file for SSH access to remote hosts
-path_to_private_key = %(COMPUTE_PATH_TO_PRIVATE_KEY)s
-
-# Connection string to the database of Compute service
-db_uri = %(COMPUTE_DB_URI)s
-
-[image]
-# This section contains configuration options used when executing tests
-# against the OpenStack Images API
-
-# The type of endpoint for an Image API service. Unless you have a
-# custom Keystone service catalog implementation, you probably want to leave
-# this value as "image"
-catalog_type = %(IMAGE_CATALOG_TYPE)s
-
-# The version of the OpenStack Images API to use
-api_version = %(IMAGE_API_VERSION)s
-
-# This is the main host address of the Image API
-host = %(IMAGE_HOST)s
-
-# Port that the Image API is running on
-port = %(IMAGE_PORT)s
-
-# This should be the username of a user WITHOUT administrative privileges
-username = %(IMAGE_USERNAME)s
-# The above non-administrative user's password
-password = %(IMAGE_PASSWORD)s
-# The above non-administrative user's tenant name
-tenant_name = %(IMAGE_TENANT_NAME)s
-
-[compute-admin]
-# This section contains configuration options for an administrative
-# user of the Compute API. These options are used in tests that stress
-# the admin-only parts of the Compute API
-
-# This should be the username of a user WITH administrative privileges
-username = %(COMPUTE_ADMIN_USERNAME)s
-# The above administrative user's password
-password = %(COMPUTE_ADMIN_PASSWORD)s
-# The above administrative user's tenant name
-tenant_name = %(COMPUTE_ADMIN_TENANT_NAME)s
-
-[identity-admin]
-# This section contains configuration options for an administrative
-# user of the Compute API. These options are used in tests that stress
-# the admin-only parts of the Compute API
-
-# This should be the username of a user WITH administrative privileges
-username = %(IDENTITY_ADMIN_USERNAME)s
-# The above administrative user's password
-password = %(IDENTITY_ADMIN_PASSWORD)s
-# The above administrative user's tenant name
-tenant_name = %(IDENTITY_ADMIN_TENANT_NAME)s
-
-[volume]
-# This section contains the configuration options used when executing tests
-# against the OpenStack Block Storage API service
-
-# The type of endpoint for a Cinder or Block Storage API service.
-# Unless you have a custom Keystone service catalog implementation, you
-# probably want to leave this value as "volume"
-catalog_type = %(VOLUME_CATALOG_TYPE)s
-# Number of seconds to wait while looping to check the status of a
-# volume that is being made available
-build_interval = %(VOLUME_BUILD_INTERVAL)s
-# Number of seconds to time out on waiting for a volume
-# to be available or reach an expected status
-build_timeout = %(VOLUME_BUILD_TIMEOUT)s
-
-[network]
-catalog_type = %(NETWORK_CATALOG_TYPE)s
-api_version = %(NETWORK_API_VERSION)s
-"""
-
 template_grizzly = """
 [identity]
 # This section contains configuration options that a variety of Tempest
@@ -460,21 +276,17 @@ quantum_available = %(QUANTUM)s
 
 class PrepareTempest():
     def __init__(self, username, password, tenant, public_ip, internal_ip,
-                 release, simple, path_to_private_key):
+                 simple, path_to_private_key):
         self.username = username
         self.password = password
         self.tenant = tenant
         self.public_ip = public_ip
         self.internal_ip = internal_ip
-        self.release = release
         self.simple = simple
         self.path_to_private_key = path_to_private_key
 
     def prepare(self):
-        if self.release == "grizzly":
-            self.prepare_tempest_grizzly(template_grizzly)
-        else:
-            self.prepare_tempest_folsom(template_folsom)
+        self.prepare_tempest_grizzly(template_grizzly)
 
     def get_auth_url(self):
         print "auth_url", 'http://%s:5000/v2.0/' % self.public_ip
@@ -503,17 +315,6 @@ class PrepareTempest():
                                                     endpoint_type='publicURL')
         return glanceclient.Client('1', endpoint=endpoint,
                                    token=keystone.auth_token)
-
-    def prepare_tempest_folsom(self, template_folsom):
-        image_ref, image_ref_alt, net_id, router_id = self.make_tempest_objects()
-        self._tempest_write_config(
-            self._tempest_config_folsom(
-                template=template_folsom,
-                image_ref=image_ref,
-                image_ref_alt=image_ref_alt,
-                compute_db_uri='mysql://nova:nova@%s/nova' % self.internal_ip
-            )
-        )
 
     def prepare_tempest_grizzly(self, template_grizzly):
         image_ref, image_ref_alt, net_id, router_id = self.make_tempest_objects()
@@ -580,66 +381,6 @@ class PrepareTempest():
     def _tempest_write_config(self, config):
         with open('tempest.conf', 'w') as f:
             f.write(config)
-
-    def _tempest_config_folsom(
-            self, template, image_ref, image_ref_alt,
-            compute_db_uri='mysql://user:pass@localhost/nova'):
-        sample = load(template)
-        config = sample % {
-            'IDENTITY_USE_SSL': 'false',
-            'IDENTITY_HOST': self.public_ip,
-            'IDENTITY_PORT': '5000',
-            'IDENTITY_API_VERSION': 'v2.0',
-            'IDENTITY_PATH': 'tokens',
-            'IDENTITY_STRATEGY': 'keystone',
-            'COMPUTE_ALLOW_TENANT_ISOLATION': 'true',
-            'COMPUTE_ALLOW_TENANT_REUSE': 'true',
-            'USERNAME': 'tempest1',
-            'PASSWORD': 'secret',
-            'TENANT_NAME': 'tenant1',
-            'ALT_USERNAME': 'tempest2',
-            'ALT_PASSWORD': 'secret',
-            'ALT_TENANT_NAME': 'tenant2',
-            'IMAGE_ID': image_ref,
-            'IMAGE_ID_ALT': image_ref_alt,
-            'FLAVOR_REF': '1',
-            'FLAVOR_REF_ALT': '2',
-            'COMPUTE_BUILD_INTERVAL': '10',
-            'COMPUTE_BUILD_TIMEOUT': '600',
-            'RUN_SSH': 'false',
-            'NETWORK_FOR_SSH': 'novanetwork',
-            'COMPUTE_CATALOG_TYPE': 'compute',
-            'COMPUTE_CREATE_IMAGE_ENABLED': 'true',
-            'COMPUTE_RESIZE_AVAILABLE': 'true',
-            'COMPUTE_CHANGE_PASSWORD_AVAILABLE': 'false',
-            'COMPUTE_LOG_LEVEL': 'DEBUG',
-            'COMPUTE_WHITEBOX_ENABLED': 'true',
-            'COMPUTE_SOURCE_DIR': '/opt/stack/nova',
-            'COMPUTE_CONFIG_PATH': '/etc/nova/nova.conf',
-            'COMPUTE_BIN_DIR': '/usr/local/bin',
-            'COMPUTE_PATH_TO_PRIVATE_KEY': self.path_to_private_key,
-            'COMPUTE_DB_URI': compute_db_uri,
-            'IMAGE_CATALOG_TYPE': 'image',
-            'IMAGE_API_VERSION': '1',
-            'IMAGE_HOST': self.public_ip,
-            'IMAGE_PORT': '9292',
-            'IMAGE_USERNAME': 'tempest1',
-            'IMAGE_PASSWORD': 'secret',
-            'IMAGE_TENANT_NAME': 'tenant1',
-            'COMPUTE_ADMIN_USERNAME': self.username,
-            'COMPUTE_ADMIN_PASSWORD': self.password,
-            'COMPUTE_ADMIN_TENANT_NAME': self.tenant,
-            'IDENTITY_ADMIN_USERNAME': self.username,
-            'IDENTITY_ADMIN_PASSWORD': self.password,
-            'IDENTITY_ADMIN_TENANT_NAME': self.tenant,
-            'VOLUME_CATALOG_TYPE': 'volume',
-            'VOLUME_BUILD_INTERVAL': '10',
-            'VOLUME_BUILD_TIMEOUT': '300',
-            'NETWORK_CATALOG_TYPE': 'network',
-            'NETWORK_API_VERSION': 'v2.0',
-        }
-
-        return config
 
     def _tempest_config_grizzly(
             self, template, image_ref, image_ref_alt,
@@ -738,8 +479,6 @@ def load(path):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--release", help="openstack release under test",
-                        default="grizzly")
     parser.add_argument("--username", help="administrator name",
                         default="admin")
     parser.add_argument("--password", help="administrator password",
@@ -763,7 +502,6 @@ def main():
         public_ip=args['public_ip'],
         internal_ip=args['internal_ip'],
         simple=args['simple'],
-        release=args['release'],
         path_to_private_key=args['key']
     )
 
