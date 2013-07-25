ae54c7235e47899816430c3134df53862866a2dd
Victor Galkin <vicglarson@gmail.com>
2013-07-25 23:56:03 +0400

    Update nova_destroy_all.py util

--- stress/tools/nova_destroy_all.py
+++ stress/tools/nova_destroy_all.py
@@ -16,30 +16,37 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-from novaclient.v1_1 import client
+from novaclient.v1_1 import client as nova_client
+import novaclient.exceptions
+from cinderclient.v2 import client as cinder_client
 import tempest.config
 
 # get the environment variables for credentials
 identity = tempest.config.TempestConfig().identity
 
-nt = client.Client(identity.username, identity.password,
-                   identity.tenant_name, identity.uri)
+nt = nova_client.Client(identity.username, identity.password,
+                        identity.tenant_name, identity.uri)
+cc = cinder_client.Client(identity.username, identity.password,
+                     identity.tenant_name, identity.uri)
 
 flavor_list = nt.flavors.list()
 server_list = nt.servers.list()
 images_list = nt.images.list()
 keypairs_list = nt.keypairs.list()
 floating_ips_list = nt.floating_ips.list()
-volumes_list = nt.volumes.list()
+try:
+    volumes_list = nt.volumes.list()
+except novaclient.exceptions.NotFound:
+    volumes_list = cc.volumes.list(detailed=False)
 
 print "total servers: %3d, total flavors: %3d, total images: %3d," % \
-    (len(server_list),
-     len(flavor_list),
-     len(images_list)),
+      (len(server_list),
+       len(flavor_list),
+       len(images_list)),
 
 print "total keypairs: %3d, total floating ips: %3d" % \
-    (len(keypairs_list),
-     len(floating_ips_list))
+      (len(keypairs_list),
+       len(floating_ips_list))
 
 print "deleting all servers"
 for s in server_list:
